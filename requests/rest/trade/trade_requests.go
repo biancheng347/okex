f1f23fe36d1be4c93237c152a1ff56556a29663a
jackwang <biancheng347@163.com>
2025-12-17 16:43:36 +0800

    [fix] update algoId

--- requests/rest/trade/trade_requests.go
+++ requests/rest/trade/trade_requests.go
@@ -148,7 +148,7 @@ type (
 	}
 	CancelAlgoOrder struct {
 		InstID string `json:"instId"`
-		AlgoID string `json:"AlgoId"`
+		AlgoID string `json:"algoId"`
 	}
 	AlgoOrderList struct {
 		InstType okex.InstrumentType `json:"instType,omitempty"`
